5a6bb0794e0ce1c3d8c778f0ca5c5abad2fdf85c
jarrett.brown <jarrett.brown@daugherty.com>
2023-11-27 10:43:50 -0600

    init commit - cell v1

---
+++ cell_v1/data.js
@@ -0,0 +1,105 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cell-exterior-p",
+      "name": "Cell-Exterior-p",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.6651256364801608,
+        "pitch": -0.1674601440508532,
+        "fov": 1.4083983627516938
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11233670017903918,
+          "pitch": -0.16045611523168013,
+          "rotation": 0,
+          "target": "1-cell-interior_o"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cell-interior_o",
+      "name": "Cell-Interior_o",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.04462498632043932,
+        "pitch": -0.23620084755491533,
+        "fov": 1.4083983627516938
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.711906896876906,
+          "pitch": -0.23126663248167567,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.5314124696120874,
+          "pitch": -0.3589756600482268,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.2945942501810581,
+          "pitch": -1.1165720757298931,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.1569238188963737,
+          "pitch": -0.08860760611601393,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Escape_Room_Cell_V1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
