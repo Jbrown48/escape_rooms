d68de10affc9986445f9382dfe018d1ff2c651db
jarrett.brown <jarrett.brown@daugherty.com>
2023-11-27 13:56:21 -0600

    mess with hotspots

--- cell_v2/data.js
+++ cell_v2/data.js
@@ -1,144 +1,148 @@
 var APP_DATA = {
-  "scenes": [
+  scenes: [
     {
-      "id": "0-cell-exterior-p",
-      "name": "Cell-Exterior-p",
-      "levels": [
+      id: "0-cell-exterior-p",
+      name: "Cell-Exterior-p",
+      levels: [
         {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
         },
         {
-          "tileSize": 512,
-          "size": 512
+          tileSize: 512,
+          size: 512,
         },
         {
-          "tileSize": 512,
-          "size": 1024
+          tileSize: 512,
+          size: 1024,
         },
         {
-          "tileSize": 512,
-          "size": 2048
-        }
+          tileSize: 512,
+          size: 2048,
+        },
       ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 0.6651256364801608,
-        "pitch": -0.1674601440508532,
-        "fov": 1.4083983627516938
+      faceSize: 2048,
+      initialViewParameters: {
+        yaw: 0.6651256364801608,
+        pitch: -0.1674601440508532,
+        fov: 1.4083983627516938,
       },
-      "linkHotspots": [
+      hintHotspots: [],
+      linkHotspots: [
         {
-          "yaw": 0.11233670017903918,
-          "pitch": -0.16045611523168013,
-          "rotation": 6.283185307179586,
-          "target": "1-cellextjpg1"
-        }
+          yaw: 0.11233670017903918,
+          pitch: -0.16045611523168013,
+          rotation: 6.283185307179586,
+          target: "1-cellextjpg1",
+        },
       ],
-      "infoHotspots": []
+      infoHotspots: [],
     },
     {
-      "id": "1-cellextjpg1",
-      "name": "cellextjpg1",
-      "levels": [
+      id: "1-cellextjpg1",
+      name: "cellextjpg1",
+      levels: [
         {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
         },
         {
-          "tileSize": 512,
-          "size": 512
+          tileSize: 512,
+          size: 512,
         },
         {
-          "tileSize": 512,
-          "size": 1024
+          tileSize: 512,
+          size: 1024,
         },
         {
-          "tileSize": 512,
-          "size": 2048
-        }
+          tileSize: 512,
+          size: 2048,
+        },
       ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 1.3470768499483405,
-        "pitch": 0.485564588455631,
-        "fov": 1.4083983627516938
+      faceSize: 2048,
+      initialViewParameters: {
+        yaw: 1.3470768499483405,
+        pitch: 0.485564588455631,
+        fov: 1.4083983627516938,
       },
-      "linkHotspots": [],
-      "infoHotspots": [
+      linkHotspots: [],
+      infoHotspots: [
         {
-          "yaw": 0.27927905914096485,
-          "pitch": 0.22048940109651838,
-          "title": "2",
-          "text": "Text"
+          yaw: 0.27927905914096485,
+          pitch: 0.22048940109651838,
+          title: "2",
+          text: "Text",
         },
         {
-          "yaw": 2.0041296720462167,
-          "pitch": -0.08433627105903696,
-          "title": "5",
-          "text": "Text"
+          yaw: 2.0041296720462167,
+          pitch: -0.08433627105903696,
+          title: "5",
+          text: "Text",
         },
         {
-          "yaw": 2.637840475840685,
-          "pitch": 0.7926580742764049,
-          "title": "4",
-          "text": "Text"
+          yaw: 2.637840475840685,
+          pitch: 0.7926580742764049,
+          title: "4",
+          text: "This is a <a href='https://www.google.com'>hyperlink</a> within text.",
         },
         {
-          "yaw": 1.3910219728777253,
-          "pitch": 0.4441902697709761,
-          "title": "1",
-          "text": "Text"
+          yaw: 1.3910219728777253,
+          pitch: 0.4441902697709761,
+          title: "1",
+          text: "Text",
         },
         {
-          "yaw": 1.307022792172976,
-          "pitch": -0.7264842562172973,
-          "title": "6",
-          "text": "Text"
+          yaw: 1.307022792172976,
+          pitch: -0.7264842562172973,
+          title: "6",
+          text: "Text",
         },
+      ],
+      hintHotspots: [
         {
-          "yaw": 2.1488315385591905,
-          "pitch": 0.35967489299094524,
-          "title": "3",
-          "text": "Text"
-        }
-      ]
+          yaw: 2.1488315385591905,
+          pitch: 0.35967489299094524,
+          title: "3",
+          text: "something - www.google.com",
+        },
+      ],
     },
     {
-      "id": "2-cellextjpg2",
-      "name": "cellextjpg2",
-      "levels": [
+      id: "2-cellextjpg2",
+      name: "cellextjpg2",
+      levels: [
         {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
         },
         {
-          "tileSize": 512,
-          "size": 512
+          tileSize: 512,
+          size: 512,
         },
         {
-          "tileSize": 512,
-          "size": 1024
-        }
+          tileSize: 512,
+          size: 1024,
+        },
       ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+      faceSize: 1024,
+      initialViewParameters: {
+        pitch: 0,
+        yaw: 0,
+        fov: 1.5707963267948966,
       },
-      "linkHotspots": [],
-      "infoHotspots": []
-    }
+      hintHotspots: [],
+      linkHotspots: [],
+      infoHotspots: [],
+    },
   ],
-  "name": "Escape_Room_Cell_V2",
-  "settings": {
-    "mouseViewMode": "drag",
-    "autorotateEnabled": false,
-    "fullscreenButton": true,
-    "viewControlButtons": false
-  }
+  name: "Escape_Room_Cell_V2",
+  settings: {
+    mouseViewMode: "drag",
+    autorotateEnabled: false,
+    fullscreenButton: true,
+    viewControlButtons: false,
+  },
 };
